9f40c3caf232cf4144258a1090b91ab4a2bd5e33
robajillo <rjillo@yahoo.com>
2020-11-01 12:58:24 +0300

    imported required files

--- neighborapp/models.py
+++ neighborapp/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+import datetime
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django_currentuser.db.models import CurrentUserField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Neighborhood(models.Model):
     name = models.CharField(max_length=250, blank=True)
