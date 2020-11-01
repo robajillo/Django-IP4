c29f9e76c2489da282423c0a38ee2f4d087ef7e2
robajillo <rjillo@yahoo.com>
2020-11-01 15:06:55 +0300

    created PostForm

--- neighborapp/forms.py
+++ neighborapp/forms.py
@@ -24,3 +24,8 @@ class BusinessForm(forms.ModelForm):
     class Meta:
         model = Business
         exclude = ('user', 'neighborhood')
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('user', 'hood')
