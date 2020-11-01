855eb7b7461a3490c8495a98179caeb813f0cc1c
robajillo <rjillo@yahoo.com>
2020-11-01 15:04:09 +0300

    created HoodForm

--- neighborapp/forms.py
+++ neighborapp/forms.py
@@ -9,3 +9,8 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class HoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighborhood
+        exclude = ('admin',)
