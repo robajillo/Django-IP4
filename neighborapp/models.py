29ae965f1260a14932d55d4293968d200131c66a
robajillo <rjillo@yahoo.com>
2020-11-01 13:04:51 +0300

    created class Profile

--- neighborapp/models.py
+++ neighborapp/models.py
@@ -11,7 +11,27 @@ class Neighborhood(models.Model):
     location = models.CharField(max_length=250, blank=True)
     occupants = models.IntegerField(default=0)
     admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
-    profile_picture = models.ImageField(upload_to='images/')
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
     
-    
+    def __str__(self):
+        return f'{self.name} hood'
+
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
+    @classmethod
+    def find_neighborhood(cls, neighborhood_id):
+        return cls.objects.filter(id=neighborhood_id)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=50, blank=True)
+    location = models.CharField(max_length=50, blank=True)
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.SET_NULL, related_name='members', blank=True)
+    profile_picture = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return f'{self.user.username} profile'
