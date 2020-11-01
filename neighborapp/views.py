e38268eb9bbc38431777c94ead592fcc115dc9fb
robajillo <rjillo@yahoo.com>
2020-11-01 13:38:25 +0300

    defined leave_hood view function

--- neighborapp/views.py
+++ neighborapp/views.py
@@ -77,3 +77,9 @@ def join_hood(request, id):
     request.user.profile.neighbourhood = neighbourhood
     request.user.profile.save()
     return redirect('hood')
+
+def leave_hood(request, id):
+    hood = get_object_or_404(NeighbourHood, id=id)
+    request.user.profile.neighbourhood = None
+    request.user.profile.save()
+    return redirect('hood')
