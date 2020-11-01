91fb818b084c1d9664d0465d189e4a3892b2444c
robajillo <rjillo@yahoo.com>
2020-11-01 13:17:51 +0300

    imported required files to views.py

--- neighborapp/views.py
+++ neighborapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import *
+from django.contrib.auth.models import User
 
-# Create your views here.
