2b5fe6bab7bcef3ccafc0a92a53daf481523655a
robajillo <rjillo@yahoo.com>
2020-11-01 13:30:59 +0300

    defined single_neighborhood view function

--- neighborapp/views.py
+++ neighborapp/views.py
@@ -35,3 +35,34 @@ def new_hood(request):
     else:
         form = HoodForm()
     return render(request, 'new_hood.html', {'form': form})
+
+def neighborhood(request):
+    all_hoods = Neighborhood.objects.all()
+    all_hoods = all_hoods[::-1]
+    params = {
+        'all_hoods': all_hoods,
+    }
+    return render(request, 'neighborhood.html', params)
+
+def single_neighborhood(request, hood_id):
+    hood = Neighborhood.objects.get(id=hood_id)
+    business = Business.objects.filter(neighborhood=hood)
+    posts = Post.objects.filter(hood=hood)
+    posts = posts[::-1]
+    if request.method == 'POST':
+        form = BusinessForm(request.POST)
+        if form.is_valid():
+            business_form = form.save(commit=False)
+            business_form.neighbourhood = hood
+            business_form.user = request.user.profile
+            business_form.save()
+            return redirect('single-neighborhood', hood.id)
+    else:
+        form = BusinessForm()
+    params = {
+        'hood': hood,
+        'business': business,
+        'form': form,
+        'posts': posts
+    }
+    return render(request, 'single_neighborhood.html', params)
