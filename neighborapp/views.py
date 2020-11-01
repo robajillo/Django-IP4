ac2687b0e3b3d33793514a627adc5ee6b0e22dce
robajillo <rjillo@yahoo.com>
2020-11-01 13:19:16 +0300

    defined signup view function

--- neighborapp/views.py
+++ neighborapp/views.py
@@ -9,3 +9,17 @@ from django.contrib.auth.models import User
 @login_required(login_url='login')
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
