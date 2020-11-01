bd24ff5776ab50cda624f2daf27fcaf0b0df6537
robajillo <rjillo@yahoo.com>
2020-11-01 12:57:38 +0300

    created class neighborhood

--- neighborapp/models.py
+++ neighborapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=250, blank=True)
+    location = models.CharField(max_length=250, blank=True)
+    occupants = models.IntegerField(default=0)
+    admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
+    profile_picture = models.ImageField(upload_to='images/')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    
+    
